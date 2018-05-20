b4612bd460fce10fd1b6dac76b0e0fb8ecd957dc
Brandi-Ciro <crbn@hotmail.it>
2018-05-20 20:13:19 +0200

    Implementazione della funzione di back propagation con funzione di errore: somma dei quadrati. aggiunta di un ulteriore parametro nel costruttore della classe NET, tale parametro è il discriminante della funzione di errore. Il parametro inserito non è stato utilizzato nella prima versione della funzione di back prop

--- Main.py
+++ Main.py
@@ -5,13 +5,13 @@ import numpy as np
 
 
 print("Started building the Feed Forward Full Connected Neural Network! \n")
-my_net = N.Net(5, [5, 4], [1, 1])
+my_net = N.Net(5, [5, 4, 4], [1, 1, 1], 0)
 #my_net.print()
 
 test_X = np.array([1, 2, 3, 4, 5])
-test_T = 1 - 2 * np.random.rand(1, 5)
+test_T = 1 - 2 * np.random.rand(1, 4)
 
-my_net.backpro_tss(test_X, test_T)
+my_net.backpro_tss(test_X, test_T[0])
 
 
 print("Closing the script, bye bye! \n")
--- Net.py
+++ Net.py
@@ -29,21 +29,26 @@ class Net:
     # Avrò i layer 1 e 2 (hidden layers) con sigmoide e
     # il layer di uscita con identità.
 
+    # err_fun è la scelta per la funzione di errore
+    # 0: somma dei quadrati
+    # 1: cross entropy
+
     # Raises ValueError exception if values of neurons_list
     # or actfun_list contain errors. (e.g. activation function
     # not existent)
-    def __init__(self, n_f, neurons_list, actfun_list):
+    def __init__(self, n_f, neurons_list, actfun_list, err_fun):
         try:
             if(len(neurons_list) == len(actfun_list)):
 
                 self.n_features = n_f
                 self.array_layers = []
                 self.n_layers = len(neurons_list)
-
+                # valore della funzione di errore discriminante
+                self.err_fun = err_fun
                 """All'inizio n_prev, ovvero il numero dei nodi dello strato
                 precedente corrisponderà al numero degli input X1, ..., Xd"""
                 n_prev = self.n_features
-
+                #TODO Controllo sui neuroni dello strato di output se sono uguali al target
                 for i in range(0, len(neurons_list)):
 
                     n_nodes = neurons_list[i]
@@ -101,29 +106,34 @@ class Net:
 
     # Funzione che calcola la i delta per ogni layer
     # per la rete con funzione di errore TSS (Total Sum of Squares.)
-    def backpro_tss(self, X, T):
+    def backpro_tss(self, x, t):
 
         # Effettua la forward per l'input X
-        Y = self.forward(X)
+        y = self.forward(x)
 
         # Il delta per lo strato di output è
         # uguale a -------> f'(a) * (Y - T)
         # Y è l'array di output, T quello dei labels (e.g. [0, 0, 0, 1])
-        self.array_layers[self.n_layers - 1].delta = \
-            np.dot(self.array_layers[self.n_layers - 1].actfun_der(Y), (Y - T))
+        # print(y)
+        # print(t)
+        self.array_layers[self.n_layers-1].delta = self.err_sum_of_squares_delta_k(y, t)
+        # print(self.array_layers[self.n_layers-1].delta)
 
+        print("dimensione della rete"+str(len(self.array_layers)));
         # Calcola il delta per i layer a ritroso.
-        for i in range(self.n_layers -1, -1, -1):
+        for i in range(self.n_layers -2, -1, -1):
+            print("iterazione"+str(i))
 
-            # delta_temporaneo = W ^ i .* D ^ i-1
-            # calcolato con prodotto punto-punto.
-            delta = np.dot(self.array_layers[i + 1].delta,
-                           self.array_layers[i].weights_matrix)
+            # delta{l}=delta{l+1}*miaNet.W{l+1};
+            tmp = self.array_layers[i+1].z * (1-self.array_layers[i+1].z)
+            # delta{l}=miaNet.output{l} .* (1-miaNet.output{l}) .*delta{l};
+            delta = np.dot(np.dot(self.array_layers[i].z, (1-self.array_layers[i].z)), tmp)
+            self.array_layers[i].delta = delta
+            print("delta "+ str(i))
+            print(self.array_layers[i].delta)
 
-            # D ^ i = delta_temporaneo .*
-            self.array_layers[i].delta = np.dot(self.array_layers[i].actfun_der(self.array_layers[i].z),
-                                                delta)
-
-
+    def err_sum_of_squares_delta_k(self, y, t):
+        # delta_out=  Y .* (1-Y) .* (Y-T);
+        return np.dot(np.dot(y, (1-y)), np.diff([y-t]))
 
 
